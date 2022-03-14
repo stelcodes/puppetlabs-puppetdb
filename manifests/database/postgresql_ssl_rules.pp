30fd0285c2c69afbc51202d2ebd2ec39c39fff23
Stel Abrego <stel@stel.codes>
2022-03-14 11:09:51 -0700

    (PDB-5379) Update postgres auth options for 14 and above
    PostgreSQL has made a breaking change to authentication options in
    version 14 so we need to check for the version and adjust the options
    accordingly.

--- manifests/database/postgresql_ssl_rules.pp
+++ manifests/database/postgresql_ssl_rules.pp
@@ -3,8 +3,17 @@ define puppetdb::database::postgresql_ssl_rules (
   String $database_name,
   String $database_username,
   String $puppetdb_server,
+  String $postgres_version
 ) {
   $identity_map_key = "${database_name}-${database_username}-map"
+
+  if versioncmp($postgres_version, "14") >= 0 {
+    # Postges version is 14 or later
+    $auth_option = "verify-full"
+  } else {
+    # Postges version is prior to 14
+    $auth_option = "map=${identity_map_key} clientcert=1"
+  }
 
   postgresql::server::pg_hba_rule { "Allow certificate mapped connections to ${database_name} as ${database_username} (ipv4)":
     type        => 'hostssl',
@@ -13,7 +22,7 @@ define puppetdb::database::postgresql_ssl_rules (
     address     => '0.0.0.0/0',
     auth_method => 'cert',
     order       => 0,
-    auth_option => "map=${identity_map_key} clientcert=1"
+    auth_option => $auth_option
   }
 
   postgresql::server::pg_hba_rule { "Allow certificate mapped connections to ${database_name} as ${database_username} (ipv6)":
@@ -23,7 +32,7 @@ define puppetdb::database::postgresql_ssl_rules (
     address     => '::0/0',
     auth_method => 'cert',
     order       => 0,
-    auth_option => "map=${identity_map_key} clientcert=1"
+    auth_option => $auth_option
   }
 
   postgresql::server::pg_ident_rule { "Map the SSL certificate of the server as a ${database_username} user":
